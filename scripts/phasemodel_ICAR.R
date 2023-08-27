409ea08ca797352e7bd4de1ca3a3afb900759b7b
Alexes Mes <akm73@cam.ac.uk>
2023-08-27 12:41:10 +0100

    Further cumulative friction ideas

--- scripts/phasemodel_ICAR.R
+++ scripts/phasemodel_ICAR.R
@@ -25,56 +25,56 @@ load(here('data','c14data_farmers.RData'))
 
 #===============================================================================
 # MCMC RunScript (Model 1 Basic without site interdependence or aggregation up to an area level) ----
-model1 <- nimbleCode({
-  for (i in 1:n_dates)
-  {
-    theta[i] ~ dunif(max=a, min=b); #where a and b are the start and end dates of the focus area (switched around because BP dates go in the positive direction for nimbleCarbon)
-    # Calibration
-    mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]); #C14 age on the relevant calibration curve
-    sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]); #error on the calibration curve
-    error[i] <- (cra_error[i]^2 + sigmaCurve[i]^2)^(1/2); #the samples' C14 error + error on calibration curve
-    cra[i] ~ dnorm(mean=mu[i], sd=error[i]); #observed radiocarbon age of the sample
-  }
-  a ~ dunif(5000,12000);
-  b ~ dunif(5000,12000);
-  unif.const ~ dconstraint(b<a); #date a is earlier than date b
-})
-
-
-#Initialise parameters ---- 
-constants1 <- list(n_dates = f_constants$n_dates,
-                   calBP = intcal20$CalBP,
-                   C14BP = intcal20$C14Age,
-                   C14err = intcal20$C14Age.sigma)
-
-d1 <- list(cra = f_dateInfo$cra,
-           cra_error = f_dateInfo$cra_error,
-           unif.const=1)
-
-theta.init = medCal(calibrate(d1$cra, 
-                              d1$cra_error, 
-                              verbose = FALSE))
-
-inits1 <- list(a=12000,
-               b=7000,
-               theta=theta.init)
-
-
-#Run MCMC ----
-mcmc.samples1<- nimbleMCMC(code = model1,
-                           constants = constants1,
-                           data = d1,
-                           niter = 200, 
-                           nchains = 3, 
-                           thin=10, 
-                           nburnin = 100,
-                           monitors=c('a','b','theta'), 
-                           inits=inits1, 
-                           samplesAsCodaMCMC=TRUE)
-
-#Diagnostics ----
-rhat1  <- gelman.diag(mcmc.samples1, multivariate = FALSE)
-ess1  <- effectiveSize(mcmc.samples1)
+# model1 <- nimbleCode({
+#   for (i in 1:n_dates)
+#   {
+#     theta[i] ~ dunif(max=a, min=b); #where a and b are the start and end dates of the focus area (switched around because BP dates go in the positive direction for nimbleCarbon)
+#     # Calibration
+#     mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]); #C14 age on the relevant calibration curve
+#     sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]); #error on the calibration curve
+#     error[i] <- (cra_error[i]^2 + sigmaCurve[i]^2)^(1/2); #the samples' C14 error + error on calibration curve
+#     cra[i] ~ dnorm(mean=mu[i], sd=error[i]); #observed radiocarbon age of the sample
+#   }
+#   a ~ dunif(5000,12000);
+#   b ~ dunif(5000,12000);
+#   unif.const ~ dconstraint(b<a); #date a is earlier than date b
+# })
+# 
+# 
+# #Initialise parameters ---- 
+# constants1 <- list(n_dates = f_constants$n_dates,
+#                    calBP = intcal20$CalBP,
+#                    C14BP = intcal20$C14Age,
+#                    C14err = intcal20$C14Age.sigma)
+# 
+# d1 <- list(cra = f_dateInfo$cra,
+#            cra_error = f_dateInfo$cra_error,
+#            unif.const=1)
+# 
+# theta.init = medCal(calibrate(d1$cra, 
+#                               d1$cra_error, 
+#                               verbose = FALSE))
+# 
+# inits1 <- list(a=12000,
+#                b=7000,
+#                theta=theta.init)
+# 
+# 
+# #Run MCMC ----
+# mcmc.samples1<- nimbleMCMC(code = model1,
+#                            constants = constants1,
+#                            data = d1,
+#                            niter = 200, 
+#                            nchains = 3, 
+#                            thin=10, 
+#                            nburnin = 100,
+#                            monitors=c('a','b','theta'), 
+#                            inits=inits1, 
+#                            samplesAsCodaMCMC=TRUE)
+# 
+# #Diagnostics ----
+# rhat1  <- gelman.diag(mcmc.samples1, multivariate = FALSE)
+# ess1  <- effectiveSize(mcmc.samples1)
 
 #===============================================================================
 ##MODEL 2 -- Add in areas
@@ -85,6 +85,8 @@ dat <- list(cra = f_dateInfo$cra,
             cra_error = f_dateInfo$cra_error,
             constraint_uniform = rep(1, f_constants$n_areas),
             cra_constraint = rep(1, f_constants$n_dates)) # Set-up constraint for ignoring inference outside calibration range
+
+origin_site <- f_siteInfo %>% filter(siteID==f_constants$origin_siteID)
 
 # Initial parameters --
 buffer <- 100
@@ -121,56 +123,56 @@ init_b  <- init_b[ ,2] - buffer
 f_constants$id_date_area <- f_constants$id_area[f_constants$id_sites] #indexing determines which area k date i belongs to. 
   
 #------------------------------------------------------------
-model2 <- nimbleCode({
-  for (i in 1:n_dates)
-  {
-    theta[i] ~ dunif(max=a[id_date_area[i]], min=b[id_date_area[i]]); # Alternatively k=id_area[id_sites[i]] -- but, dynamic indexing not allowed
-    # Calibration
-    mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]); #C14 age on the relevant calibration curve
-    sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]); #error on the calibration curve
-    error[i] <- (cra_error[i]^2 + sigmaCurve[i]^2)^(1/2); #the samples' C14 error + error on calibration curve
-    cra[i] ~ dnorm(mean=mu[i], sd=error[i]); #observed radiocarbon age of the sample
-  }
-
-  # Set Prior for Each Region
-  for (k in 1:n_areas){
-    a[k] ~ dunif(4000, 12000);
-    b[k] ~ dunif(4000, 12000);
-    constraint_uniform[k] ~ dconstraint(a[k]>b[k]) #In each area, start date of occupation, a_k, must be greater than the end date of occupation, b_k (note: BP dates in the positive direction)
-  }
-})
-
-
-#Initialise parameters ---- 
-constants2 <- list(n_dates = f_constants$n_dates,
-                   n_areas = f_constants$n_areas,
-                   id_date_area = f_constants$id_date_area,
-                   calBP = intcal20$CalBP,
-                   C14BP = intcal20$C14Age,
-                   C14err = intcal20$C14Age.sigma)
-
-
-
-inits2 <- list(a=init_a,
-               b=init_b,
-               theta=theta_init)
-
-
-#Run MCMC ----
-mcmc.samples2<- nimbleMCMC(code = model2,
-                           constants = constants2,
-                           data = dat,
-                           niter = 200, 
-                           nchains = 3, 
-                           thin=10, 
-                           nburnin = 100,
-                           monitors=c('a','b','theta'), 
-                           inits=inits2, 
-                           samplesAsCodaMCMC=TRUE)
-
-#Diagnostics ----
-rhat2  <- gelman.diag(mcmc.samples2, multivariate = FALSE)
-ess2  <- effectiveSize(mcmc.samples2)
+# model2 <- nimbleCode({
+#   for (i in 1:n_dates)
+#   {
+#     theta[i] ~ dunif(max=a[id_date_area[i]], min=b[id_date_area[i]]); # Alternatively k=id_area[id_sites[i]] -- but, dynamic indexing not allowed
+#     # Calibration
+#     mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]); #C14 age on the relevant calibration curve
+#     sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]); #error on the calibration curve
+#     error[i] <- (cra_error[i]^2 + sigmaCurve[i]^2)^(1/2); #the samples' C14 error + error on calibration curve
+#     cra[i] ~ dnorm(mean=mu[i], sd=error[i]); #observed radiocarbon age of the sample
+#   }
+# 
+#   # Set Prior for Each Region
+#   for (k in 1:n_areas){
+#     a[k] ~ dunif(4000, 12000);
+#     b[k] ~ dunif(4000, 12000);
+#     constraint_uniform[k] ~ dconstraint(a[k]>b[k]) #In each area, start date of occupation, a_k, must be greater than the end date of occupation, b_k (note: BP dates in the positive direction)
+#   }
+# })
+# 
+# 
+# #Initialise parameters ---- 
+# constants2 <- list(n_dates = f_constants$n_dates,
+#                    n_areas = f_constants$n_areas,
+#                    id_date_area = f_constants$id_date_area,
+#                    calBP = intcal20$CalBP,
+#                    C14BP = intcal20$C14Age,
+#                    C14err = intcal20$C14Age.sigma)
+# 
+# 
+# 
+# inits2 <- list(a=init_a,
+#                b=init_b,
+#                theta=theta_init)
+# 
+# 
+# #Run MCMC ----
+# mcmc.samples2<- nimbleMCMC(code = model2,
+#                            constants = constants2,
+#                            data = dat,
+#                            niter = 200, 
+#                            nchains = 3, 
+#                            thin=10, 
+#                            nburnin = 100,
+#                            monitors=c('a','b','theta'), 
+#                            inits=inits2, 
+#                            samplesAsCodaMCMC=TRUE)
+# 
+# #Diagnostics ----
+# rhat2  <- gelman.diag(mcmc.samples2, multivariate = FALSE)
+# ess2  <- effectiveSize(mcmc.samples2)
 
 #===============================================================================
 ##MODEL 3 -- Model arrival time with additional variables
@@ -281,71 +283,94 @@ sq_grid_conc <- sq_grid_conc %>%
 #The weights, w_f and w_e, currently ensure that sq_fit_org and sq_eleve_org are of the same order of magnitude
 #TODO: explore these assumptions and give weights to each of these parameters based on the relative importance
 
-w_e <- 0.25 #To refine. Arrived at by looking at (max(sq_grid_conc$sq_elev_org)-min(sq_grid_conc$sq_elev_org)) compared to (max(sq_grid_conc$sq_dist_org)-min(sq_grid_conc$sq_dist_org))
-w_f <- 200 #To refine. Arrived at by looking at (max(sq_grid_conc$sq_fit_org)-min(sq_grid_conc$sq_fit_org)) compared to (max(sq_grid_conc$sq_dist_org)-min(sq_grid_conc$sq_dist_org))
+w_e <- 1 #To refine. Arrived at by looking at (max(sq_grid_conc$sq_elev_org, na.rm=TRUE)-min(sq_grid_conc$sq_elev_org, na.rm=TRUE)) compared to (max(sq_grid_conc$sq_dist_org, na.rm=TRUE)-min(sq_grid_conc$sq_dist_org, na.rm=TRUE))
+w_f <- 600 #To refine. Arrived at by looking at (max(sq_grid_conc$sq_fit_org, na.rm=TRUE)-min(sq_grid_conc$sq_fit_org, na.rm=TRUE)) compared to (max(sq_grid_conc$sq_dist_org, na.rm=TRUE)-min(sq_grid_conc$sq_dist_org, na.rm=TRUE))
   
 sq_grid_conc <- sq_grid_conc %>% 
   mutate(cf = sq_dist_org - w_f*sq_fit_org + w_e*sq_elev_org) %>% 
   mutate(cf = case_when(cf<0 ~ 50, cf>=0 ~ cf)) #If cumulative friction is negative -- assign a small positive value. It takes a minimum amount of effort to move. #TODO: adjust amount. Currently chosen at random.
 
 
- 
+#-------------------------------------------------------------------------------
+## Store Output ----
+save(sq_grid_conc, file=here('output', 'cumulative_friction.RData'))
+     
+     
 #-------------------------------------------------------------------------------  
-model3 <- nimbleCode({
-  for (i in 1:n_dates)
-  {
-    theta[i] ~ dunif(max=a[id_date_area[i]], min=b[id_date_area[i]]); # Alternatively k=id_area[id_sites[i]] -- but, dynamic indexing not allowed
-    # Calibration
-    mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]); #C14 age on the relevant calibration curve
-    sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]); #error on the calibration curve
-    error[i] <- (cra_error[i]^2 + sigmaCurve[i]^2)^(1/2); #the samples' C14 error + error on calibration curve
-    cra[i] ~ dnorm(mean=mu[i], sd=error[i]); #observed radiocarbon age of the sample
-  }
-  
-  # Set Prior for Each Region
-  for (k in 1:n_areas){
-    sigma[k] ~ dcar_normal(adj[1:L], weights[1:L], num[k], tau1, zero_mean =0); #spatial random effect modelled using ICAR
-    
-    a[k] ~ beta_1 + sigma[k];
-    b[k] ~ dunif(4000, 12000);
-    constraint_uniform[k] ~ dconstraint(a[k]>b[k]) #In each area, start date of occupation, a_k, must be greater than the end date of occupation, b_k (note: BP dates in the positive direction)
-  }
-  tau1 <- 1/sigma1^2
-  sigma1 ~ dunif(0,100)
-  
-})
-
-
-
-#Initialise parameters ---- 
-constants3 <- list(n_dates = f_constants$n_dates,
-                   n_areas = f_constants$n_areas,
-                   id_date_area = f_constants$id_date_area,
-                   calBP = intcal20$CalBP,
-                   C14BP = intcal20$C14Age,
-                   C14err = intcal20$C14Age.sigma)
-
-
-
-inits3 <- list(a=init_a,
-               b=init_b,
-               theta=theta_init)
-
-
-#Run MCMC ----
-mcmc.samples3<- nimbleMCMC(code = model3,
-                           constants = constants3,
-                           data = dat,
-                           niter = 200, 
-                           nchains = 3, 
-                           thin=10, 
-                           nburnin = 100,
-                           monitors=c('a','b','theta'), 
-                           inits=inits3, 
-                           samplesAsCodaMCMC=TRUE)
-
-#Diagnostics ----
-rhat3  <- gelman.diag(mcmc.samples3, multivariate = FALSE)
-ess3  <- effectiveSize(mcmc.samples3)
-
-
+# model3 <- nimbleCode({
+#   for (i in 1:n_dates)
+#   {
+#     theta[i] ~ dunif(max=a[id_date_area[i]], min=b[id_date_area[i]]); # Alternatively k=id_area[id_sites[i]] -- but, dynamic indexing not allowed
+#     # Calibration
+#     mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]); #C14 age on the relevant calibration curve
+#     sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]); #error on the calibration curve
+#     error[i] <- (cra_error[i]^2 + sigmaCurve[i]^2)^(1/2); #the samples' C14 error + error on calibration curve
+#     cra[i] ~ dnorm(mean=mu[i], sd=error[i]); #observed radiocarbon age of the sample
+#   }
+#   
+#   # Set Prior for Each Region
+#   for (k in 1:n_areas){
+#     #sigma[k] ~ dcar_normal(adj[L], weights[L], num[k], tau1, zero_mean =0); #spatial random effect modelled using ICAR
+#     cf[k] ~ dunif(0, 1000);
+#     a[k] <- beta_0 + beta_1*cf[k] #beta_0 + (beta_1+sigma[k])*cf[k] #T(dnorm(beta_0, sigma), min = 4000, max=12000); #dnorm(beta_0 + beta_1*cf[k], sigma); #(beta_1 + sigma[k])*c[k];
+#     b[k] ~ dunif(4000, 12000);
+#     constraint_uniform[k] ~ dconstraint(a[k]>b[k]) #In each area, start date of occupation, a_k, must be greater than the end date of occupation, b_k (note: BP dates in the positive direction)
+#   }
+#   
+#   #priors
+#   beta0 ~ dnorm(8000, sd=200);
+#   beta1 ~ dexp(1);
+#   tau1 <- 1/sigma1^2;
+#   sigma ~ dunif(0,100); #dexp(0.01)
+#   #sigma1 ~ dunif(0,100)
+#   
+# })
+# 
+# 
+# # x <- seq(2000, 14000, length=10000)
+# # y <- dunif(x, 7000, 9000)
+# # z <- dnorm(x, mean=y, sd=200)
+# # plot(x, z, type="l", lwd=1)
+# 
+# 
+# 
+# # MCMC initialisation ----
+# set.seed(1357)
+# inits3  <-  list()
+# inits3$theta  <- theta_init
+# inits3$beta0 <- rnorm(1, 8000, 200)
+# inits3$beta1 <- rexp(1, rate=2)
+# inits3$a <- init_a
+# inits3$b <- init_b
+# inits3$cf <- sq_grid_conc$cf
+# #inits3$sigma1 <- runif(1,0,100)
+# inits3$sigma  <- rexp(1, 0.01)
+# #inits3$sigma <- rnorm(f_constants$n_areas, sd=0.001)
+#   
+# #Initialise parameters ---- 
+# constants3 <- list(n_dates = f_constants$n_dates,
+#                    n_areas = f_constants$n_areas,
+#                    id_date_area = f_constants$id_date_area,
+#                    calBP = intcal20$CalBP,
+#                    C14BP = intcal20$C14Age,
+#                    C14err = intcal20$C14Age.sigma)
+# 
+# 
+# 
+# #Run MCMC ----
+# mcmc.samples3<- nimbleMCMC(code = model3,
+#                            constants = constants3,
+#                            data = dat,
+#                            niter = 200, 
+#                            nchains = 3, 
+#                            thin=30, 
+#                            nburnin = 100,
+#                            monitors=c('a','b','theta'), 
+#                            inits=inits3, 
+#                            samplesAsCodaMCMC=TRUE)
+# 
+# #Diagnostics ----
+# rhat3  <- gelman.diag(mcmc.samples3, multivariate = FALSE)
+# ess3  <- effectiveSize(mcmc.samples3)
+# 
+# 
